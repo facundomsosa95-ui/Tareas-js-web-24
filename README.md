5e3a74d2c5893faf7eed9230a2648546b38fe925
Pablo Marino <pmarino2013@gmail.com>
2026-06-09 08:56:52 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Teoría y práctica módulo 2 - Comisión Web24
+
+Repositorio con la teoría y práctica del módulo 2 del curso de desarrollo web de la comisión Web24.
---
+++ js/01-variables.js
@@ -0,0 +1,91 @@
+//variables
+// var nombreCompleto = 'Joaquin Perez'
+
+let alumna22 = "Josefina";
+const PRIMER_DIA_SEMANA = "Lunes";
+let $cosita = "";
+// let class = 'Pepito' NO VA
+$cosita;
+PRIMER_DIA_SEMANA;
+//que el nombre de las variables sea lo más descriptivo posible.
+let marcaDelVehiculo = "Ford";
+
+let alumnita = "Pepa";
+
+//nombre de variables
+// - letras mayúsculas y minúsculas del alfabeto Inglés
+// - números del 0 al 9
+// - signos '$' y '_'
+
+//No debemos usar
+// No podemos iniciar el nombre con números
+//no puedo usar palabras reservadas
+
+//Tipos de datos
+
+//Number
+let numeroPositivo = 34;
+let numeroNegativo = -900;
+let numeroDecimal = 89.7;
+
+//String (texto)
+let academia = "Rolling Code";
+let numeroString = "22";
+let stringVacio = "";
+
+//Boolean
+let comprar = true; //1
+let comerEnCasa = false; //0
+
+//undefined
+let cosita;
+console.log(cosita);
+
+//Null (vacio)
+let billeteraDePablo = null;
+
+//NaN (not a number)
+console.log(numeroPositivo);
+console.log(numeroPositivo + comprar);
+
+//Para saber que tipo de dato tiene una variable:
+typeof billeteraDePablo;
+/*
+Datos primitivos (se almacenan directamente en la memoria)
+
+Number: Números enteros o decimales.
+String: Cadenas de texto.
+Boolean: Valores lógicos (true o false).
+Undefined: Indica la ausencia de un valor.
+Null: Representa un valor intencionalmente vacío.
+NaN: Not a Number
+
+Datos no primitivos (se almacenan en referencias en la memoria)
+
+Object: Colecciones de pares clave-valor.
+Array: Listas ordenadas de valores.
+Function: Bloques de código reutilizables.
+Date: Representaciones de fechas y horas.
+RegExp: Expresiones regulares para buscar patrones en cadenas.
+Error: Representaciones de errores y excepciones.
+ */
+
+/*Ejercicios
+
+1 - Siguiendo buenas prácticas que palabra/s reservada/s de javascript debemos usar para declarar variables. 
+
+2 - Declarar e inicializar una variable de tipo String, una de tipo booleana y dos de tipo number.
+
+3 - ¿Con qué  convención de nomenclatura está escrito el nombre de la siguente variable (pascal case, camel case o snake case)?
+
+let cantidadDeAlumnos=45 camelCase
+
+4 - Qué tipo de datos tienen almacenados las siguientes variables?
+
+ - const MASCOTAS=['Boby','Chatran','Scooby']
+ - let comisionActiva
+ - let estaInactiva= false
+
+5 - ¿Cómo puedo saber que tipo de datos tiene una variable desde la consola?. 
+
+*/
