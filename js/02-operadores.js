e96c0c82c8d0600f9f076de96e5703c2668b1baf
Pablo Marino <pmarino2013@gmail.com>
2026-06-09 10:17:44 -0300

    Agrego documentación básica de operadores y condicionales

---
+++ js/02-operadores.js
@@ -0,0 +1,35 @@
+//Operadores
+
+/*
+Operadores matemáticos
+-------------------------
++ suma
+- resta
+* multiplicación
+/ división
+% resto
+
+console.log(); // imprime en consola lo que le pasemos entre paréntesis
+
+Operadores Lógicos
+-------------------
+&& AND
+|| OR
+! NOT
+
+Operadores de relación
+-----------------------
+> mayor
+< menor
+>= mayor o igual
+<= menor o igual
+!= distinto de
+== igualación
+=== igualación estricta
+
+
+Operadores unitarios
+---------------------
+++ incremento
+-- decremento
+*/
---
+++ js/03-condicional.js
@@ -0,0 +1,28 @@
+// Estructuras condicionales
+
+/*
+Condicional simple
+-------------------
+if (condition) {
+    acciones
+}
+
+Condicional básico
+------------------
+if (condition) {
+    acciones
+} else {
+    acciones
+}  
+    
+Condicional anidado
+-------------------
+if(condition){
+    acciones
+}else if(condition){
+    acciones
+}else{
+    acciones
+}
+
+*/
